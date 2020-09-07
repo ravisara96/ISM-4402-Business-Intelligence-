4f5a93382c76557ca99449bc1db36cd5db35cea8
ravisara96 <70700671+ravisara96@users.noreply.github.com>
2020-09-08 00:07:25 +0530

    Add files via upload

---
+++ Workbook 1.2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+Location = "/Users/sachiwairugalbandara/Desktop/Python Files/households.xls"
+df = pd.read_excel(Location)
+
+
+# In[10]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
