7d46d180db1ff6a0b0052bd4b2deec617c581c1e
ravisara96 <70700671+ravisara96@users.noreply.github.com>
2020-09-07 23:41:29 +0530

    Add files via upload

---
+++ Workbook 1.1 .py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+df = pd.read_csv (r'/Users/sachiwairugalbandara/Desktop/Python Files/Florida.csv') 
+print (df)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
